0e663b51e310b32bd23c02419c8f7e89c59251f8
joseweslley03 <weslleyphb03@gmail.com>
2024-06-14 21:15:01 -0300

    Alteração para banco de dados

--- produtos/models.py
+++ produtos/models.py
@@ -1,40 +1,55 @@
 from pyexpat import model
 from django.db import models
+from utils.base_models import BaseModel
 
-class Categoria(models.Model):
+class Categoria(BaseModel):
     nome = models.CharField(max_length=100, verbose_name='nome da categoria', unique=True)
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data de criação da categoria')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data de atualização da categoria')
 
     class Meta:
-        tb_table = 'categorias'
+        db_table = 'categorias'
 
-class Produto(models.Model):
+class Produto(BaseModel):
     nome = models.CharField(max_length=100, verbose_name='nome do produto')
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='categoria do produto')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data da criação do produto')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data de atualização do produto')
+    embalagens = models.ManyToManyField('produtos.Embalagem',verbose_name='Embalagens do produto')
 
     class Meta:
-        tb_table = 'produtos'
+        db_table = 'produtos'
 
-class Fornecedor(models.Model):
+class Fornecedor(BaseModel):
     nome_social = models.CharField(max_length=100, verbose_name='Razao Social do fornecedor', unique=True)
     nome_fantasia = models.CharField(max_length=100, verbose_name='nome Fantasia do fornecedor')
-    produtos =models.ManyToManyField(Produto, verbose_name='Produtos do Fornecedor', through='FornecedorProduto', )
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data da criação do fornecedor')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data de atualização do fornecedor')
+    produtos =models.ManyToManyField(Produto, verbose_name='Produtos do Fornecedor',)
+    
     
     class Meta:
-        tb_table = 'fornecedores'
+        db_table = 'fornecedores'
 
-class FornecedorProduto(model.Model):
-    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE, verbose_name='fornecedor do produto')
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='produto do fornecedor')
-    preco = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço do produto')
-    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='data da criação')
-    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='data de atualização')
+class Embalagem(BaseModel):
+    name = models.CharField(max_length=50, verbose_name='Nome da embalagem')
+    sigla = models.CharField(max_length=3,verbose_name='Sigla da embalagem')
 
+    class Meta:
+        db_table = 'embalagens'
+
+class Movimentacao(BaseModel):
+    TIPOS_MOVIMENTACAO =[(1, 'Entrada'), (-1, 'Saida'),]
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='produto da movimento')
+    fornecedor = models.ForeignKey('produtos.Fornecedor', on_delete=models.CASCADE, verbose_name='Fornecedor do produto movimentado',)
+    quantidade = models.DecimalField(max_digits=10, decimal_places=6, verbose_name='Quantidade movimentada')
+    local = models.ForeignKey('produtos.Local', on_delete=models.CASCADE, verbose_name='Local da movimentação')
+    tipo = models.IntegerField(choices=TIPOS_MOVIMENTACAO, verbose_name='Tipo de movimentação')
+
+    class Meta:
+        db_table = 'movimentacoes'
+
+class Local(BaseModel):
+    TIPOS_DE_LOCAL = [('F', 'Fisico'), ('D', 'Digital')]
+    nome = models.CharField(max_length=50, verbose_name='Nome do local armazenado')
+    tipo = models.CharField(max_length=1, choices=TIPOS_DE_LOCAL)
+
+    class Meta:
+        db_table = 'locais'
 
 
 
