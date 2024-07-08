0555bb69b5ff252c67e0d06e114645034148f143
joseweslley03 <weslleyphb03@gmail.com>
2024-07-08 18:56:57 -0300

    edicao

--- produtos/forms.py
+++ produtos/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Embalagem, Local
+from .models import Categoria, Embalagem, Fornecedor, Local, Produto
 
 
 class LocalForm(forms.ModelForm):
@@ -11,4 +11,19 @@ class LocalForm(forms.ModelForm):
 class EmbalagemForm(forms.ModelForm):
     class Meta:
         model = Embalagem
-        fields = ['nome', 'tipo']
+        fields = ['nome', 'sigla']
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = Categoria
+        fields = ['nome']
+
+class FornecedorForm(forms.ModelForm):
+    class Meta:
+        model = Fornecedor
+        fields = ['nome_social', 'nome_fantasia', 'produtos']
+
+class ProdutosForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'categoria', 'embalagens', 'estoque_minimo', 'estoque_maximo']  # noqa: E501
